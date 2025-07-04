da9bbaf1bf5e6a7e1192c19dd494e19ecb4da018
Younis Said <Younissaid900@gmail.com>
2025-07-04 15:16:27 +0300

    Added Task 1 and 2

---
+++ Task-1/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Program
+{
+    //Data Types
+    public struct BirthDate
+    {
+        public int day { get; set; }
+        public int month { get; set; }
+        public int year { get; set; }
+    }
+    public class Person
+    {
+        public int ID { get; set; }
+        public BirthDate birthDate { get; set; }
+    }
+    public enum WeeKDays
+    {
+        Sunday = 1,
+        Monday = 2,
+        Tuesday = 3,
+        Wednesday = 4,
+        Thursday = 5,
+        Friday = 6,
+        Saturday = 7
+    }
+    //Reference Type
+    class Point
+    {
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Point() { } // Default constructor
+
+        public int x { get; set; }
+        public int y { get; set; }
+        //inheritaed methods
+        public override string ToString()
+        {
+            return $"{x} ,{y}";
+        }
+        //public override bool Equals(object obj)
+        //{
+        //    Point other = obj as Point;
+        //    return this.x == other.x && this.y == other.y;
+
+
+        //}
+    }
+    class Program
+    {
+        static void Main()
+        {
+            //comments
+            //int x = 10;
+            /*   */
+            print();
+
+            //Variables Declartion
+            int x;//Declare
+            x = 10;//initailaize
+            //Data Types
+            Person person = new Person();
+            //Value Type
+            int Y = 20; // Value Type
+            Y++; // Y = 21
+
+            //Reference Type
+            Point P1 = new Point();
+            P1.x = 10;
+            P1.y = 20;
+
+            Point P2 = new Point(30, 40);
+            P2 = new Point(50, 60); // different Refrence
+
+            //inheritaed methods
+            #region Object
+            Point P3 = new Point(10, 20);
+            Console.WriteLine(P3.ToString()); // Prints the type Fullname by default
+
+            Point P4 = new Point(10, 20);
+            Point P5 = new Point(10, 20);
+            Console.WriteLine(P4.Equals(P5)); //default compares the references not the values
+
+            String Name1 = "Ali";
+            String Name2 = "Ali";
+            Console.WriteLine(Name1.Equals(Name2));//he found the same value so he put them in the same reference in heap
+            Console.WriteLine(Name1.GetHashCode());
+            Console.WriteLine(Name2.GetHashCode()); //same value same hashcode
+
+
+            Point P6 = new Point(10, 20);
+            Console.WriteLine(P6.GetType().FullName);
+
+            //Object
+            object obj = new Point(10, 20);
+            obj = 5;
+            obj = "Hello World";
+            obj = 10.5;
+            obj = true;
+
+            #endregion
+
+            Console.WriteLine(P1.x);
+            Console.WriteLine(P1.y);
+
+
+
+
+            Console.ReadKey();
+        }
+        //comments//
+        /// <summary>
+        /// Print
+        /// </summary>
+        static void print()
+        {
+
+        }
+    }
+}
---
+++ Task-2/Program.cs
@@ -0,0 +1,163 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Runtime.InteropServices;
+using System;
+using System.Reflection.Metadata;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        //public class Point
+        //{
+        //   public int X { get; set; }
+        //   public int Y { get; set; }
+
+        //   public Point(int x, int y)
+        //    {
+        //        X = x;
+        //        Y = y;
+        //    }
+        //}
+
+        static void Main(string[] args)
+        {
+            //1 Write a program that allows the user to enter a number then print it.
+            //Console.WriteLine("Please Enter a Num");
+            //int Num = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Your Number is {Num}");
+
+            //2 Write C# program that converts a string to an integer, but the string contains non-numeric characters. And mention what will happen 
+            //string str = "10A";
+            //int Num = Convert.ToInt32(str); // Exception String was not in correct Format
+
+            //3 Write C# program that Perform a simple arithmetic operation with floating-point numbers And mention what will happen
+            //float N1 = 0.1f;
+            //float N2 = 0.2f;
+            //float Result = N1 + N2;
+            //Console.WriteLine(N1 + N2);
+
+            //4 Write C# program that Extract a substring from a given string.
+            //string Name = "Younis Said";
+            //string subName = Name.Substring(0, 6);
+            //Console.WriteLine(subName);
+
+            //5 Write C# program that Assigning one value type variable to another and modifying the value of one variable and mention what will happen
+            //int x = 10;
+            //int y = x;   
+            //y = 20;      
+            //Console.WriteLine("x = " + x);  // x is still 10
+            //Console.WriteLine("y = " + y);  // y is 20
+
+            //6 Write C# program that Assigning one reference type variable to another and modifying the object through one variable and mention what will happen
+            //Point P1 = new Point(10, 20);
+            //Point P2 = new Point(30, 50);
+            //P1 = P2;
+            //Console.WriteLine(P1.X); // X is 30
+            //Console.WriteLine(P2.X); // Y is 30
+
+            //7 Write C# program that take two string variables and print them as one variable
+            //string firstName = "Younis";
+            //string lastName = "Said";
+            //string fullName = firstName + " " + lastName;
+            //Console.WriteLine("Full Name: " + fullName);
+
+            //8Write a program that calculates the simple interest given the principal amount, rate of interest, and time.The formula for simple interest is
+            //Interest = (principal * rate * time) / 100.
+            //Console.Write("Enter Principal Amount: ");
+            //double principal = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Enter Rate of Interest : ");
+            //double rate = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Enter Time (in years): ");
+            //double time = Convert.ToDouble(Console.ReadLine());
+
+
+            //double interest = (principal * rate * time) / 100;
+
+            //Console.WriteLine("Interest = " + interest);
+
+            //9.Write a program that calculates the Body Mass Index(BMI) given a person's weight in kilograms and height in meters. The formula for BMI is
+            //BMI = (Weight) / (Height * Height)
+            //Console.Write("Enter your weight in kilograms: ");
+            //double weight = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Enter your height in meters: ");
+            //double height = Convert.ToDouble(Console.ReadLine());
+
+            //double BMI = weight / (height * height);
+
+            //Console.WriteLine("Your BMI is: " + BMI);
+
+            //10. Write a program that uses the ternary operator to check if the temperature is too hot, too cold, or just good. Assign the result in a variable then display the result. Assume that below 10 degrees is "Just Cold", above 30 degrees is "Just Hot", and anything else is "Just Good".
+            //Console.WriteLine("Please Enter Temperature : ");
+            //int Temperature = Convert.ToInt32(Console.ReadLine());
+            //string Result = Temperature < 10 ? "Just Cold" : Temperature > 30 ? "Just Hot" : "Just Good";
+            //Console.WriteLine("Temperature Status : " +Result);
+            //Console.ReadKey();
+
+
+            // 11 Write a program that takes the date from the user and displays it in various formats using string interpolation.
+            //Ex:
+            //            Today’s date : 20 , 11 , 2001
+            //Today's date : 20 / 11 / 2001
+            //Today's date : 20 – 11 – 2001
+            //Console.Write("Enter day: ");
+            //int day = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Enter month: ");
+            //int month = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Enter year: ");
+            //int year = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine($" Date : {day} , {month} , {year}");
+            //Console.WriteLine($" Date : {day} / {month} / {year}");
+            //Console.WriteLine($" Date : {day} – {month} – {year}");
+
+            //12 What is the output of the following C# code?
+            //DateTime date = new DateTime(2024, 6, 14); Console.WriteLine($"The event is on {date:MM/dd/yyyy}"); //The event is on 06/14/2024
+
+            //13Which of the following statements is correct about the C#.NET code snippet given below?
+            //int d;
+            //d = Convert.ToInt32(!(30 < 20));
+            //Console.WriteLine(d);// A value 1 will be assigned to d.
+
+            //14 Which of the following is the correct output for the C# code given below?
+            //Console.WriteLine(13 / 2 + " " + 13 % 2); //6 1
+
+            // 15 - What will be the output of the C# code given below?
+            //int num = 1, z = 5;
+            //if (!(num <= 0))
+            //    Console.WriteLine(++num + z++ + " " + ++z); // 7 7
+            //else
+            //    Console.WriteLine(--num + z-- + " " + --z);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
