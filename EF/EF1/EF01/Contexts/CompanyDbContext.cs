e21e5569cb73b1a2573cac5e890cd397fa14282e
Younis Said <Younissaid900@gmail.com>
2025-09-09 16:25:52 +0300

    ADDED DEMO

---
+++ EF/EF1/EF01/Contexts/CompanyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using EF01.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF01.Contexts
+{
+    public class CompanyDbContext : DbContext
+    {
+        public CompanyDbContext() : base() { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "server=.; database=CompanyDb; trusted_connection=true; TrustServerCertificate=True;");
+        }
+
+        public DbSet<Employee> Employees { get; set; } //table
+
+    }
+}
