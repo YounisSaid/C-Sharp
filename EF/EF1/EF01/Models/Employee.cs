5a649782c69221ff32071936705ed2392f62f0e1
Younis Said <Younissaid900@gmail.com>
2025-09-09 16:02:16 +0300

    Added DEMO

---
+++ EF/EF1/EF01/Models/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF01.Models
+{
+    ////By convection
+    //public class Employee
+    //{
+    //    public int Id { get; set; } // public numeric ID or EmployeeID (PK+identity(1,1))
+    //    public string?  Name { get; set; } // nvarchar(max)
+    //}
+
+    //By Data Annoation
+    public class Employee
+    {
+        public int Id { get; set; }
+        //[MaxLength(100)]
+        [Length(10, 200)]
+        [Column(TypeName = "varchar")]
+
+        public string Name { get; set; }
+
+        [AllowedValues(10, 20, 30)]
+        [DeniedValues(10, 020, 30)]
+        [Range(10, 30)]
+        [DefaultValue(null)]
+        public decimal? Salary { get; set; }
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+
+        [NotMapped]
+        public int test { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string Address { get; set; }
+
+    }
+
+}
