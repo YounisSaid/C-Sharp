34e3d6b2286aabeb56ad6f2e972aa9a23aa5ee6b
Younis Said <Younissaid900@gmail.com>
2025-09-20 16:35:04 +0300

    Added EF03

---
+++ EF/EF03/EF03/DEMO/Contexts/CompanyDbContext.cs
@@ -0,0 +1,51 @@
+﻿using DEMO.Models;
+using EF01.Models;
+
+//using EF01.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF01.Contexts
+{
+    public class CompanyDbContext : DbContext
+    {
+        public CompanyDbContext() :base() { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "server=.; database=CompanyDb003; trusted_connection=true; TrustServerCertificate=True;")
+                          .UseLazyLoadingProxies();
+
+         
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Course>()
+            //            .HasMany(x => x.Students)
+            //            .WithMany(x => x.Courses);
+
+            modelBuilder.Entity<StudentCourse>().HasKey(x => new { x.StudentId, x.CourseId });
+
+            modelBuilder.Entity<Department>().HasData(
+                                                        new Department { DeptId = 1, DeptName = "Pr", DateOfCreation = DateTime.Now },
+                                                        new Department { DeptId = 1, DeptName = "Pr", DateOfCreation = DateTime.Now },
+                                                        new Department { DeptId = 1, DeptName = "Pr", DateOfCreation = DateTime.Now }
+
+                                                     );
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+        public DbSet<Employee> Employees { get; set; } //table
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet <Course> Courses { get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+    }
+}
---
+++ EF/EF03/EF03/DEMO/Program.cs
@@ -0,0 +1,37 @@
+﻿using DEMO.Models;
+using EF01.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace DEMO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using CompanyDbContext companyDb = new CompanyDbContext();
+            //companyDb.AddRange(new Department { DeptId = 1, DeptName = "Pr", DateOfCreation = DateTime.Now },
+            //                                            new Department { DeptId = 1, DeptName = "Pr", DateOfCreation = DateTime.Now },
+            //                                            new Department { DeptId = 1, DeptName = "Pr", DateOfCreation = DateTime.Now });
+            //var employee = companyDb.Employees.First();
+            //if(employee != null)
+            //{
+            //    Console.WriteLine(employee.Department?.DeptName);
+            //}
+            //var employee = companyDb.Employees.Include(x=>x.Department).ThenInclude(x=>x.employees).First();
+            //companyDb.Entry(employee).Reference(x=>x.Department).Load();
+            //var employee = companyDb.Employees.First(x=>x.DepartmentId ==1);
+            //Console.WriteLine(employee.Department.DeptName);    
+
+            var employee = from Employee in companyDb.Employees
+                           join Department in companyDb.Departments
+                           on Employee.DepartmentId equals Department.DeptId
+                           select Employee;
+
+            var result = companyDb.Employees.Join(companyDb.Departments, employee => employee.DepartmentId, Department => Department.DeptId, (employee, Department) => new
+            { });
+
+
+        }
+
+    }
+}
